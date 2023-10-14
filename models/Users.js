8175d1d8efc604943be025f91a8198f02c54715f
Houssem Dodor <houssem@dodor.nl>
2023-10-14 16:14:09 +0200

    Added Register and login functionality

--- models/Users.js
+++ models/Users.js
@@ -8,6 +8,26 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pwd: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
 
   return Users;
--- routes/Users.js
+++ routes/Users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
+const bcrypt = require("bcrypt");
 
 router.get("/", async (req, res) => {
   const listofUsers = await Users.findAll();
@@ -9,8 +10,30 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const user = req.body;
-  await Users.create(user);
-  res.json(user);
+  bcrypt.hash(user.pwd, 10).then((hash) => {
+    Users.create({
+      username: user.username,
+      pwd: hash,
+      email: user.email,
+      name: user.name,
+      surname: user.surname,
+      salt: 10,
+    });
+  });
+
+  res.json("SUCCES");
+});
+
+router.post("/login", async (req, res) => {
+  const { username, pwd } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) return res.json({ error: "User doesn't exist" });
+
+  bcrypt.compare(pwd, user.pwd).then((match) => {
+    if (!match) res.json({ error: "Wrong username/password combination" });
+    else res.json("You logged in");
+  });
 });
 
 module.exports = router;
