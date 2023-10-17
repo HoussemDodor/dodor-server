63b3319dabff419725b378559e9b0858c4b7bb3a
Houssem Dodor <houssem@dodor.nl>
2023-10-17 17:16:05 +0200

    Added Logger and errorHandling in middleware

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require('./middleware/errorHandler')
 
 app.use(express.json());
-app.use(cors());
+app.use(logger);
+
+// TODO: CORS
+const whitelist = [
+  "http://localhost:3001",
+  "http://localhost:3000",
+  "https://app.dodor.nl",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    // TODO: REMOVE ORIGIN WHEN DEPLOYING
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccesStatus: 200,
+};
+
+
+app.use(cors(corsOptions));
 
 const db = require("./models");
 
 // Routers
 const userRouter = require("./routes/Users");
 app.use("/auth", userRouter);
+
+app.use(errorHandler);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
