087084bea1a47890ef4f4a707d483658812b77a0
Houssem Dodor <365095@student.fontys.nl>
2023-10-23 19:17:21 +0200

    Differentiated Users and Auth Routes

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions")
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require('./middleware/errorHandler')
 
 app.use(express.json());
-app.use(logger);
-
-// TODO: CORS
-const whitelist = [
-  "http://localhost:3001",
-  "http://localhost:3000",
-  "https://app.dodor.nl",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    // TODO: REMOVE ORIGIN WHEN DEPLOYING
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccesStatus: 200,
-};
-
-
 app.use(cors(corsOptions));
+app.use(logger);
+app.use(errorHandler);
 
 const db = require("./models");
 
 // Routers
-const userRouter = require("./routes/Users");
-app.use("/auth", userRouter);
-
-app.use(errorHandler);
+app.use("/auth", require("./routes/Auth"));
+app.use("/users", require("./routes/Users"));
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
--- routes/Users.js
+++ routes/Users.js
@@ -1,48 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
-const bcrypt = require("bcrypt");
-const { validateToken } = require("../middleware/AuthMiddleware");
 
-const { sign } = require("jsonwebtoken");
-
-router.get("/", validateToken, async (req, res) => {
+router.get("/", async (req, res) => {
   const listofUsers = await Users.findAll();
   res.json(listofUsers);
 });
 
-router.post("/", async (req, res) => {
-  const user = req.body;
-  bcrypt.hash(user.pwd, 10).then((hash) => {
-    Users.create({
-      username: user.username,
-      pwd: hash,
-      email: user.email,
-      name: user.name,
-      surname: user.surname,
-      salt: 10,
-    });
-  });
-
-  res.json("SUCCES");
-});
-
-router.post("/login", async (req, res) => {
-  const { username, pwd } = req.body;
-  const user = await Users.findOne({ where: { username: username } });
-
-  if (!user) return res.json({ error: "User doesn't exist" });
-
-  bcrypt.compare(pwd, user.pwd).then((match) => {
-    if (!match) res.json({ error: "Wrong username/password combination" });
-    else {
-      const accestoken = sign(
-        { username: user.username, id: user.id },
-        "CHANGE_THIS_SECRET_LATER"
-      );
-      res.json({ accestoken: accestoken });
-    }
-  });
-});
 
 module.exports = router;
