c48fbc5a34410f7ec3e2f0daf33d28c31cdce5e0
Houssem Dodor <365095@student.fontys.nl>
2023-10-24 00:42:38 +0200

    Removed unnecesary fields

--- models/Users.js
+++ models/Users.js
@@ -7,27 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     pwd: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    pwd: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    surname: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    salt: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+    }
   });
 
   return Users;
